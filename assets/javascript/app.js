f1f3cd949fb95c26570217f5dbe0c5c7de6328f7
Joshua Duncan <jduncan97@gmail.com>
2020-04-19 18:51:29 -0600

    Adding JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+$("#aboutDiv").hide()
+$("#portfolioDiv").hide()
+$("#contactDiv").hide()
+
+
+$("#aboutLink").on("click", function () {about()});
+$("#portfolioLink").on("click", function () {portfolio()});
+$("#contactLink").on("click", function () {contact()});
+
+function about(){
+    $("#portfolioDiv").hide()
+    $("#contactDiv").hide()
+    $("#aboutDiv").show()
+}
+function portfolio(){
+    $("#aboutDiv").hide()
+    $("#contactDiv").hide()
+    $("#portfolioDiv").show()
+}
+function contact(){
+    $("#portfolioDiv").hide()
+    $("#contactDiv").hide()
+    $("#contactDiv").show()
+}
+
